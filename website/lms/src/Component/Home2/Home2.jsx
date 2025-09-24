550bb7a95d2ac198583422523831ad75b9e700a7
Yadhukrishna123 <yadhumv365@gmail.com>
2025-09-24 10:59:13 -0700

    set responsive

--- website/lms/src/Component/Home2/Home2.jsx
+++ website/lms/src/Component/Home2/Home2.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
+import "./Home2.css"
 
 const Home2 = () => {
     return (
         <div className='mt-5'>
             <div className="w-[85%] bg-blue-200 h-[500px] flex mx-auto  bg-[url('https://askproject.net/studdy/wp-content/uploads/sites/43/2021/12/studio-shot-of-cute-thankful-clever-student-feels-RP65PDL-1.png')] bg-no-repeat bg-left bg-contain">
-                <div className='w-[40%] mt-2 flex flex-col items-center justify-center'>
+                
 
-                </div>
-
-                <div className='w-[60%] mt-2 flex flex-col items-center justify-center'>
+                <div className='section_two'>
                     <div>
-                        <h2 className='text-5xl text-center'>
+                        <h2 className='text-5xl text-center first-text'>
                             Providing Amazing Online Courses</h2>
                     </div>
                     <div className='mt-5'>
-                        <p className='w-140 leading-normal text-gray-500'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dicta, corporis sint cumque officiis quos repudiandae .</p>
+                        <p className='w-140 leading-normal text-gray-500 second-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dicta, corporis sint cumque officiis quos repudiandae .</p>
                     </div>
                 </div>
             </div>
